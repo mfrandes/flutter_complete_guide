59fbf36269aa94736fea1876ff0cea71b53b5193
Frandes Mircea <mircea.frandes@gloobus.it>
2021-02-16 13:15:15 +0200

    implement stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext contex) {
+    List<String> questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal'
+    ];
+
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => answerQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => answerQuestion(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
